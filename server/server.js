26cfabdfb88611d8df8945aa8c2e2dc652351e4b
stefalber <stefalber@gmail.com>
2018-04-15 17:31:03 +0200

    Start deploy chat-app

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.send(__dirname + "index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port 3000${port}`);
+});
